74805dd1a1bd3a50a45483e7e458ace0de132a8a
Felipe Santos <lipsantos013@gmail.com>
2022-06-28 23:35:04 -0300

    exercicio2

---
+++ evento.js
@@ -0,0 +1,26 @@
+let dataDoEvento = 31/06/2022
+
+if (dataDoEvento < 31/06/2022) {
+    console.log ("Data invalida, cadastro não permitido")
+}
+else {
+    console.log ("Data valida, cadastro realizado")
+}
+
+let idade = 20
+
+if (idade < 18) {
+    console.log ("Cadastro não permitido, idade tem que ser superior a 18")
+}
+else {
+    console.log ("Cadastro permitido, idade minima atingida")
+}
+
+let quantidadeDeParticipantes = 50
+
+if (quantidadeDeParticipantes < 100) {
+    console.log ("Cadastro realizado")
+}
+else {
+    console.log ("Quantidade de pessoas acima do permitido, cadastro invalido")
+}
